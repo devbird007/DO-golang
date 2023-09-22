f8f55a189f5cf736cd699a3066f126da4ad8e8e4
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2023-09-22 21:03:17 +0100

    methods

---
+++ methods/method.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Creature struct {
+	Name     string
+	Greeting string
+}
+
+func (c Creature) Greet() {
+	fmt.Printf("%s says %s\n", c.Name, c.Greeting)
+}
+
+func main() {
+	sammy := Creature{
+		Name:     "Sammy",
+		Greeting: "Hello!",
+	}
+	sammy.Greet() // Or Creature.Greet(sammy)
+}
