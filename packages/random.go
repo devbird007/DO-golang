2863394f42653b98fb3972d5c360e24958f0a847
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2023-09-30 16:19:00 +0100

    uuid package

--- packages/random.go
+++ packages/random.go
@@ -1,8 +1,13 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func main() {
+	now := time.Now()
+	println("Numbers seeded using current date/time:", now.Format(time.StampNano))
 	for i := 0; i < 5; i++ {
 		println(rand.Intn(10))
 	}
---
+++ packages/uuid/go.mod
@@ -0,0 +1,5 @@
+module github.com/devbird007/random
+
+go 1.21.0
+
+require github.com/google/uuid v1.3.1
