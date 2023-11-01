33131d2e1b44d5ea47ccb9ff916b6f60ec65530b
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2023-11-01 12:50:00 +0100

    method pointer receivers

--- methods/interfaces/main.go
+++ methods/interfaces/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Ocean struct {
 	Creatures []string
 }
 
-//func (o Ocean) String() string {
-//	return strings.Join(o.Creatures, ", ")
-//}
+func (o Ocean) String() string {
+	return strings.Join(o.Creatures, ", ")
+}
 
 func log(header string, s fmt.Stringer) {
 	fmt.Println(header, ":", s)
