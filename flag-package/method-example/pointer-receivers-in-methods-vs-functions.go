5b29f7e827086e6b368767a157d10d70ab5b7f21
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2023-12-18 12:02:23 +0100

    final methods

---
+++ flag-package/method-example/pointer-receivers-in-methods-vs-functions.go
@@ -0,0 +1,35 @@
+/* Pointer receivers in methods vs Pointer arguments in functions */
+package main
+
+import (
+	"fmt"
+)
+
+type rectangle struct {
+	length int
+	width  int
+}
+
+func perimeter(r *rectangle) {
+	fmt.Println("Perimeter function output:", 2*(r.length+r.width))
+}
+
+func (r *rectangle) perimeter() {
+	fmt.Println("Perimeter method output:", 2*(r.length+r.width))
+}
+
+func main() {
+	r := rectangle{
+		length: 10,
+		width:  5,
+	}
+	p := &r // pointer to r
+	perimeter(p)
+	p.perimeter()
+	/*
+		cannot use r (type rectangle) as type *rectangle in argument to perimeter
+	*/
+	//perimeter(r)
+
+	r.perimeter() //calling pointer receiver with a value
+}
